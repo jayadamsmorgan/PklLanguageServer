880ae0228a3af5983ce2f0c57525d1b19515a3c8
Herman Berdnikov <applejay1802@gmail.com>
2024-02-28 09:39:13 -0500

    Added PklIdentifier

---
+++ Sources/pkl-lsp/AST/Identifier.swift
@@ -0,0 +1,23 @@
+import Foundation
+import LanguageServerProtocol
+
+struct PklIdentifier: ASTNode {
+
+    let uniqueID: UUID = UUID()
+
+    var positionStart: Position
+    var positionEnd: Position
+
+    var value: String
+
+    init(value: String, positionStart: Position, positionEnd: Position) {
+        self.value = value
+        self.positionStart = positionStart
+        self.positionEnd = positionEnd
+    }
+
+    public func diagnosticErrors() -> [ASTDiagnosticError]? {
+        return nil
+    }
+
+}
