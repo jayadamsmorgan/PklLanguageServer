b30a3e22b64dcab29ca73fc54a4470ed5a95b14b
Herman Berdnikov <applejay1802@gmail.com>
2024-03-01 20:41:56 -0500

    Document Symbols initial

--- Sources/pkl-lsp/RequestHandlers/DocumentSymbolsHandler.swift
+++ Sources/pkl-lsp/RequestHandlers/DocumentSymbolsHandler.swift
@@ -11,8 +11,95 @@ public class DocumentSymbolsHandler {
         self.logger = logger
     }
 
+    private func getSymbols(node: any ASTNode) -> [DocumentSymbol] {
+        guard let children = node.children else {
+            return []
+        }
+        var symbols: [DocumentSymbol] = []
+        for child in children {
+            if let classNode = child as? PklClassDeclaration {
+                let classSymbol = DocumentSymbol(
+                    name: classNode.classIdentifier?.value ?? "Unknown",
+                    detail: "class",
+                    kind: .class,
+                    range: LSPRange(
+                        start: Position(line: classNode.positionStart.line, character: classNode.positionStart.character / 2),
+                        end: Position(line: classNode.positionEnd.line, character: classNode.positionEnd.character / 2)),
+                    selectionRange: LSPRange(
+                        start: Position(line: classNode.positionStart.line, character: classNode.positionStart.character / 2),
+                        end: Position(line: classNode.positionEnd.line, character: classNode.positionEnd.character / 2)),
+                    children: getSymbols(node: classNode)
+                )
+                symbols.append(classSymbol)
+            }
+            if let functionNode = child as? PklFunctionDeclaration {
+                let functionSymbol = DocumentSymbol(
+                    name: functionNode.body?.identifier?.value ?? "Unknown",
+                    detail: "function",
+                    kind: .function,
+                    range: LSPRange(
+                        start: Position(line: functionNode.positionStart.line, character: functionNode.positionStart.character / 2),
+                        end: Position(line: functionNode.positionEnd.line, character: functionNode.positionEnd.character / 2)),
+                    selectionRange: LSPRange(
+                        start: Position(line: functionNode.positionStart.line, character: functionNode.positionStart.character / 2),
+                        end: Position(line: functionNode.positionEnd.line, character: functionNode.positionEnd.character / 2)),
+                    children: getSymbols(node: functionNode)
+                )
+                symbols.append(functionSymbol)
+            }
+            if let objectNode = child as? PklObjectBody {
+                let objectSymbol = DocumentSymbol(
+                    name: "Object",
+                    detail: "object",
+                    kind: .module,
+                    range: LSPRange(
+                        start: Position(line: objectNode.positionStart.line, character: objectNode.positionStart.character / 2),
+                        end: Position(line: objectNode.positionEnd.line, character: objectNode.positionEnd.character / 2)),
+                    selectionRange: LSPRange(
+                        start: Position(line: objectNode.positionStart.line, character: objectNode.positionStart.character / 2),
+                        end: Position(line: objectNode.positionEnd.line, character: objectNode.positionEnd.character / 2)),
+                    children: getSymbols(node: objectNode)
+                )
+                symbols.append(objectSymbol)
+            }
+            if let propertyNode = child as? PklObjectProperty {
+                let propertySymbol = DocumentSymbol(
+                    name: propertyNode.identifier?.value ?? "Unknown",
+                    detail: "property",
+                    kind: .property,
+                    range: LSPRange(
+                        start: Position(line: propertyNode.positionStart.line, character: propertyNode.positionStart.character / 2),
+                        end: Position(line: propertyNode.positionEnd.line, character: propertyNode.positionEnd.character / 2)),
+                    selectionRange: LSPRange(
+                        start: Position(line: propertyNode.positionStart.line, character: propertyNode.positionStart.character / 2),
+                        end: Position(line: propertyNode.positionEnd.line, character: propertyNode.positionEnd.character / 2)),
+                    children: getSymbols(node: propertyNode)
+                )
+                symbols.append(propertySymbol)
+            }
+            if let propertyNode = child as? PklClassProperty {
+                let propertySymbol = DocumentSymbol(
+                    name: propertyNode.identifier?.value ?? "Unknown",
+                    detail: "property",
+                    kind: .property,
+                    range: LSPRange(
+                        start: Position(line: propertyNode.positionStart.line, character: propertyNode.positionStart.character / 2),
+                        end: Position(line: propertyNode.positionEnd.line, character: propertyNode.positionEnd.character / 2)),
+                    selectionRange: LSPRange(
+                        start: Position(line: propertyNode.positionStart.line, character: propertyNode.positionStart.character / 2),
+                        end: Position(line: propertyNode.positionEnd.line, character: propertyNode.positionEnd.character / 2)),
+                    children: getSymbols(node: propertyNode)
+                )
+                symbols.append(propertySymbol)
+            }
+        }
+        return symbols
+    }
+
     public func provide(document: Document, module: any ASTNode, params: DocumentSymbolParams) async -> DocumentSymbolResponse {
-        return nil
+        let symbols: [DocumentSymbol] = getSymbols(node: module)
+        logger.debug("LSP DocumentSymbols: Found \(symbols.count) symbols in \(params.textDocument.uri).")
+        return DocumentSymbolResponse(.optionA(symbols))
     }
 
 }
--- Sources/pkl-lsp/RequestHandlers/RenameHandler.swift
+++ Sources/pkl-lsp/RequestHandlers/RenameHandler.swift
@@ -14,6 +14,10 @@ public class RenameHandler {
     public func provide(document: Document, module: any ASTNode, params: RenameParams) async -> RenameResponse {
         var identifiers: [PklIdentifier] = ASTHelper.getASTIdentifiers(node: module)
         logger.debug("LSP Rename: Found \(identifiers) identifiers in \(params.textDocument.uri).")
+
+        // Character position is doubled after tree-siiter parsing due to UTF16 (I guess), so we need to divide it by 2
+        // I don't really like this solution, but it works for now
+        // Not sure if it's also a good idea to divide it by 2 at AST initialization, need to think about it
         let positionIdentifier: PklIdentifier? = identifiers.first(where: {
             $0.positionStart.line == params.position.line &&
             $0.positionStart.character / 2 <= params.position.character &&
