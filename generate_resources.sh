975b88369ce79fd1a1e0c04c41d63517f353e742
Herman Berdnikov <applejay1802@gmail.com>
2024-03-23 02:11:30 -0400

    Update fix for macOS

--- generate_resources.sh
+++ generate_resources.sh
@@ -5,12 +5,12 @@ STDLIB_PATH="pkl/stdlib"
 OUTPUT_FILE="Sources/pkl-lsp/Resources.swift"
 
 # Start the Resources struct and hashMap
-echo "// This file is auto-generated. Do not edit directly.\n" > "$OUTPUT_FILE"
+echo "// This file is auto-generated. Do not edit directly." > "$OUTPUT_FILE"
 echo "public enum Resources {" >> "$OUTPUT_FILE"
 echo "    public static let stdlib: [String: String] = [" >> "$OUTPUT_FILE"
 
 # Process each .pkl file
-find "$STDLIB_PATH" -name "*.pkl" | while IFS= read -r file; do
+while IFS= read -r file; do
     filename=$(basename -- "$file")
     echo "Processing $filename..."
     echo "        \"${filename}\": \"\"\"" >> "$OUTPUT_FILE"
@@ -21,22 +21,14 @@ find "$STDLIB_PATH" -name "*.pkl" | while IFS= read -r file; do
         echo "$escapedLine" >> "$OUTPUT_FILE"
     done < "$file"
     echo "\"\"\"," >> "$OUTPUT_FILE"
-done
+done < <(find "$STDLIB_PATH" -name "*.pkl")
 
-# Finish the hashMap and Resources struct
-# Use `head` and `tail` to remove the last comma from the hashMap
-echo "    ]" >> "$OUTPUT_FILE"
-echo "}" >> "$OUTPUT_FILE"
+# Use sed to remove the last comma. This approach is compatible across both GNU and BSD sed.
+# -i '' -e: For BSD sed compatibility, specifying an empty extension for in-place editing without backup.
+# $!N; $!ba; : For GNU sed, to accumulate the whole file into pattern space.
+# s/,\n    \]/\n    ]/: To remove the last comma before the closing square bracket.
+sed -i '' -e '$!N; $!ba; s/,\n    \]/\n    ]/' "$OUTPUT_FILE"
 
-# Correctly format the Swift dictionary to remove the last comma
-# Temporary file for intermediate output
-TMP_FILE="${OUTPUT_FILE}.tmp"
+echo "]}" >> "$OUTPUT_FILE"
 
-# Leave the last two lines (closing brackets) untouched, remove the last comma from the rest
-head -n -2 "$OUTPUT_FILE" | sed '$ s/,$//' > "$TMP_FILE"
-tail -n 2 "$OUTPUT_FILE" >> "$TMP_FILE"
-
-# Replace original file with corrected one
-mv "$TMP_FILE" "$OUTPUT_FILE"
 echo "Done!"
-
