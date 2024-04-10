205b48431eb031d06040aa8bfe9b77e352386340
Herman Berdnikov <applejay1802@gmail.com>
2024-04-10 10:04:59 -0400

    Fix typo

--- Benchmarks/PklLSPBenchmark/PklLSPBenchmark.swift
+++ Benchmarks/PklLSPBenchmark/PklLSPBenchmark.swift
@@ -9,24 +9,24 @@ let benchmarks = {
     do {
         let parser = Parser()
         try parser.setLanguage(tree_sitter_pkl())
-        let pklMathStdlib = Resources.stdlib["base.pkl"]!
+        let pklBaseStdlib = Resources.stdlib["base.pkl"]!
         Benchmark("Tree-sitter parsing") { benchmark in
             for _ in benchmark.scaledIterations {
-                blackHole(_ = parser.parse(pklMathStdlib))
+                blackHole(_ = parser.parse(pklBaseStdlib))
             }
         }
 
         Benchmark("Abstract syntax tree constructing without dependencies") { benchmark in
             for _ in benchmark.scaledIterations {
                 let astParser = TreeSitterParser(logger: Logger(label: "testLogger"), maxImportDepth: 0)
-                await blackHole(astParser.parse(document: Document(uri: "stdlib:base.pkl", version: 0, text: pklMathStdlib)))
+                await blackHole(astParser.parse(document: Document(uri: "stdlib:base.pkl", version: 0, text: pklBaseStdlib)))
             }
         }
 
         Benchmark("Abstract syntax tree construct with dependencies (maxImportDepth = 30)") { benchmark in
             for _ in benchmark.scaledIterations {
                 let astParser = TreeSitterParser(logger: Logger(label: "testLogger"), maxImportDepth: 30)
-                await blackHole(astParser.parse(document: Document(uri: "stdlib:base.pkl", version: 0, text: pklMathStdlib)))
+                await blackHole(astParser.parse(document: Document(uri: "stdlib:base.pkl", version: 0, text: pklBaseStdlib)))
             }
         }
     } catch {
